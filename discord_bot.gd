3f99e9923a5e35cbbb3610b494e90e2ef0b1cbec
Shuflduf <shuflduf@gmail.com>
2024-08-06 14:09:46 -0600

    websocket testing

--- discord_bot.gd
+++ discord_bot.gd
@@ -1,28 +1,49 @@
 class_name DiscordBot
 extends Node
 
-const BASE_URL = "https://discordapp.com/api"
-@export var token = ""
-@export var http_request: HTTPRequest
+# The URL we will connect to
+@export var websocket_url = "wss://libwebsockets.org"
+
+# Our WebSocketClient instance
+var _client = WebSocketPeer.new()
 
 func _ready():
-	http_request.request_completed.connect(_on_request_completed)
+	# Connect base signals to get notified of connection open, close, and errors.
+	_client.connection_closed.connect(_closed)
+	_client.connection_error.connect(_closed)
+	_client.connection_established.connect(_connected)
+	# This signal is emitted when not using the Multiplayer API every time
+	# a full packet is received.
+	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
+	_client.data_received.connect(_on_data)
 
-func set_token(bot_token: String):
-	token = bot_token
+	# Initiate connection to the given URL.
+	var err = _client.connect_to_url(websocket_url, TLSOptions.client())# ["lws-mirror-protocol"]
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
 
-func send_message(channel_id: String, content: String):
-	var headers = [
-		"Authorization: Bot " + token,
-		"Content-Type: application/json"
-	]
-	#var body = JSON.print({"content": content})
-	http_request.request(BASE_URL + "/channels/" + channel_id + "/messages", headers, HTTPClient.METHOD_POST, content)
+func _closed(was_clean = false):
+	# was_clean will tell you if the disconnection was correctly notified
+	# by the remote peer before closing the socket.
+	print("Closed, clean: ", was_clean)
+	set_process(false)
 
-func _on_request_completed(result, _response_code, _headers, body):
-	var json = JSON.new()
-	json.parse(body.get_string_from_utf8())
-	if json.error == OK:
-		print("Response: ", json.result)
-	else:
-		print("Error parsing JSON")
+func _connected(proto = ""):
+	# This is called on connection, "proto" will be the selected WebSocket
+	# sub-protocol (which is optional)
+	print("Connected with protocol: ", proto)
+	# You MUST always use get_peer(1).put_packet to send data to server,
+	# and not put_packet directly when not using the MultiplayerAPI.
+	_client.get_peer(1).put_packet("Test packet".to_utf8_buffer())
+
+func _on_data():
+	# Print the received packet, you MUST always use get_peer(1).get_packet
+	# to receive data from server, and not get_packet directly when not
+	# using the MultiplayerAPI.
+	print("Got data from server: ", _client.get_peer(1).get_packet().get_string_from_utf8())
+
+func _process(delta):
+	# Call this in _process or _physics_process. Data transfer, and signals
+	# emission will only happen when calling this function.
+	_client.poll()
