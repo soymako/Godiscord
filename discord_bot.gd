5dffab82616c35ade26151f0099420270809e5cb
Shuflduf <shuflduf@gmail.com>
2024-12-14 15:13:03 -0700

    try to get websocket ot work

--- discord_bot.gd
+++ discord_bot.gd
@@ -1,26 +1,50 @@
-class_name DiscordBot
 extends Node
 
+@export var token: String = OS.get_environment("DISCORD_BOT_TOKEN")
 
-@export var websocket_url = "wss://libwebsockets.org"
-
-var socket = WebSocketPeer.new()
+var websocket: WebSocketPeer
 
 func _ready():
-	socket.connect_to_url(websocket_url)
+	websocket = WebSocketPeer.new()
+	websocket.connect_to_url("wss://gateway.discord.gg/?v=9&encoding=json")
 
-func _process(delta):
-	socket.poll()
-	var state = socket.get_ready_state()
+func _process(_delta):
+	websocket.poll()
+	var state = websocket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
-		while socket.get_available_packet_count():
-			print("Packet: ", socket.get_packet())
+		while websocket.get_available_packet_count():
+			var data = websocket.get_packet().get_string_from_utf8()
+			print("Packet: ", data)
+			var json = JSON.parse_string(data)
+			if json["op"] == 10:  # Hello
+				var heartbeat_interval = json.d.heartbeat_interval / 1000.0
+				send_identify()
+				start_heartbeat(heartbeat_interval)
 	elif state == WebSocketPeer.STATE_CLOSING:
 		# Keep polling to achieve proper close.
 		pass
 	elif state == WebSocketPeer.STATE_CLOSED:
-		var code = socket.get_close_code()
-		var reason = socket.get_close_reason()
+		var code = websocket.get_close_code()
+		var reason = websocket.get_close_reason()
 		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
 		set_process(false) # Stop processing.
 
+func send_identify():
+	var payload = {
+		"op": 2,
+		"d": {
+			"token": token,
+			"intents": 513,
+			"properties": {
+				"$os": OS.get_name(),
+				"$browser": "godot",
+				"$device": "godot"
+			}
+		}
+	}
+	websocket.put_packet(JSON.stringify(payload).to_utf8_buffer())
+
+func start_heartbeat(interval: float):
+	await get_tree().create_timer(interval).timeout
+	websocket.put_packet(JSON.stringify({"op": 1, "d": null}).to_utf8_buffer())
+	start_heartbeat(interval)
