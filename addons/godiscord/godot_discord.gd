efb150473af5840a16753b582ec3106e4278301d
Shuflduf <shuflduf@gmail.com>
2024-12-15 00:24:43 -0700

    replace tabs wtih spaces

--- addons/godiscord/godot_discord.gd
+++ addons/godiscord/godot_discord.gd
@@ -2,28 +2,16 @@
 extends EditorPlugin
 
 func _enable_plugin() -> void:
-	add_autoload_singleton("DiscordRequestHandler", "scripts/discord_request_handler.gd")
+    add_autoload_singleton("DiscordRequestHandler", "scripts/discord_request_handler.gd")
 
 
 func _disable_plugin():
-	remove_autoload_singleton("DiscordRequestHandler")
+    remove_autoload_singleton("DiscordRequestHandler")
 
 
 func _enter_tree() -> void:
-	add_custom_type("DiscordResource", "Resource", preload("scripts/discord_resource.gd"), preload("assets/discord_base.svg"))
-	add_custom_type("DiscordChannel", "DiscordResource", preload("scripts/discord_channel.gd"), preload("assets/discord_base.svg"))
-	add_custom_type("DiscordCommand", "DiscordResource", preload("scripts/discord_command_request.gd"), preload("assets/discord_base.svg"))
-	add_custom_type("DiscordMessage", "DiscordResource", preload("scripts/discord_message.gd"), preload("assets/discord_base.svg"))
-	add_custom_type("DiscordUser", "DiscordResource", preload("scripts/discord_user.gd"), preload("assets/discord_base.svg"))
-
-	add_custom_type("DiscordBot", "Node", preload("scripts/discord_bot.gd"), preload("assets/discord_base.svg"))
+    add_custom_type("DiscordBot", "Node", preload("scripts/discord_bot.gd"), preload("assets/discord_base.svg"))
 
 
 func _exit_tree() -> void:
-	remove_custom_type("DiscordResource")
-	remove_custom_type("DiscordChannel")
-	remove_custom_type("DiscordCommand")
-	remove_custom_type("DiscordMessage")
-	remove_custom_type("DiscordUser")
-
-	remove_custom_type("DiscordBot")
+    remove_custom_type("DiscordBot")
--- addons/godiscord/scripts/discord_message.gd
+++ addons/godiscord/scripts/discord_message.gd
@@ -21,18 +21,18 @@ var id: int
 ## [/codeblock]
 @warning_ignore("shadowed_variable")
 func reply(content: String):
-	var url = "https://discord.com/api/v9/channels/%s/messages" % channel.id
-	var headers = [
-		"Authorization: Bot %s" % token,
-		"Content-Type: application/json"
-	]
-	var payload = {
-		"content": content,
-		"message_reference": {
-			"message_id": id
-		}
-	}
-	var http_request = HTTPRequest.new()
-	DiscordRequestHandler.add_child(http_request)
-	http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
-	http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
+    var url = "https://discord.com/api/v9/channels/%s/messages" % channel.id
+    var headers = [
+        "Authorization: Bot %s" % token,
+        "Content-Type: application/json"
+    ]
+    var payload = {
+        "content": content,
+        "message_reference": {
+            "message_id": id
+        }
+    }
+    var http_request = HTTPRequest.new()
+    DiscordRequestHandler.add_child(http_request)
+    http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+    http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
