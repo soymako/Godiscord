b9b2e462b3699ebbdbf912d87445caa00dd3aa4f
Shuflduf <shuflduf@gmail.com>
2024-12-27 19:16:56 -0700

    use tabs
    use tabs

--- addons/godiscord/scripts/discord_channel.gd
+++ addons/godiscord/scripts/discord_channel.gd
@@ -13,15 +13,15 @@ var id: int
 ##         message.channel.send_message("You said: " + message.content)
 ## [/codeblock]
 func send_message(content: String):
-    var url = "https://discord.com/api/v9/channels/%s/messages" % id
-    var headers = [
-        "Authorization: Bot %s" % token,
-        "Content-Type: application/json"
-    ]
-    var payload = {
-        "content": content
-    }
-    var http_request = HTTPRequest.new()
-    DiscordRequestHandler.add_child(http_request)
-    http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
-    http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
+	var url = "https://discord.com/api/v9/channels/%s/messages" % id
+	var headers = [
+		"Authorization: Bot %s" % token,
+		"Content-Type: application/json"
+	]
+	var payload = {
+		"content": content
+	}
+	var http_request = HTTPRequest.new()
+	DiscordRequestHandler.add_child(http_request)
+	http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+	http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
--- addons/godiscord/scripts/discord_command_request.gd
+++ addons/godiscord/scripts/discord_command_request.gd
@@ -8,18 +8,18 @@ var options: Dictionary
 var caller: DiscordUser
 
 func reply(content: String):
-    var url = "https://discord.com/api/v9/interactions/%s/%s/callback" % [interaction["id"], interaction["token"]]
-    var headers = [
-        "Authorization: Bot %s" % token,
-        "Content-Type: application/json"
-    ]
-    var payload = {
-        "type": 4,
-        "data": {
-            "content": content
-        }
-    }
-    var http_req = HTTPRequest.new()
-    DiscordRequestHandler.add_child(http_req)
-    http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
-    http_req.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+	var url = "https://discord.com/api/v9/interactions/%s/%s/callback" % [interaction["id"], interaction["token"]]
+	var headers = [
+		"Authorization: Bot %s" % token,
+		"Content-Type: application/json"
+	]
+	var payload = {
+		"type": 4,
+		"data": {
+			"content": content
+		}
+	}
+	var http_req = HTTPRequest.new()
+	DiscordRequestHandler.add_child(http_req)
+	http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
+	http_req.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
