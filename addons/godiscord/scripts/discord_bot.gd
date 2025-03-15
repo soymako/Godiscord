3e1ea26374d05a17e2c4588925351df01f70919f
SoyMako <correofacilparacosasfaciles@gmail.com>
2025-03-15 02:48:50 -0600

    Added auto-slash-command register, embeds, guild type, etc...

--- addons/godiscord/scripts/discord_bot.gd
+++ addons/godiscord/scripts/discord_bot.gd
@@ -8,7 +8,10 @@ extends Node
 ## Used to register slash commands, discords requires a GUILD_ID to register commands automatically[br]
 ## for now, this will do the trick[br][br]
 ## Right Click on the server image and press [color=aqua][i]Copy Server ID[/i][/color] and paste it here
-@export var guild_id: String
+
+
+## prints to the console
+@export var debug:bool = true;
 
 
 @onready var heartbeat_timer: Timer = Timer.new()
@@ -29,6 +32,13 @@ var intents: int = DiscordIntents.DEFAULT
 var sequence_number: int
 ## For resuming sessions and heartbeating
 
+var guilds: Array[DiscordGuild]
+
+func debugPrint(message:String)->void:
+  if debug:
+    print_rich("[color=aqua]Godiscord🎮[/color]: %s" % message)
+  pass
+
 func _ready():
   _websocket = WebSocketPeer.new()
   _websocket.connect_to_url("wss://gateway.discord.gg/?v=9&encoding=json")
@@ -36,13 +46,12 @@ func _ready():
   heartbeat_timer.one_shot = false
   heartbeat_timer.timeout.connect(_heartbeat)
 
-  if guild_id.is_empty():
-    push_warning("guild_id is empty, commands may not work!")
-  if guild_id.length() < 18 or not guild_id.is_valid_int(): # if discord changes their api, this will not work
-    push_warning("guild_id may not be a valid discord server id")
+  self.bot_ready.connect(on_ready)
+
+
+
 
   add_child(heartbeat_timer)
-
 func _process(_delta):
   _websocket.poll()
   var state = _websocket.get_ready_state()
@@ -90,6 +99,11 @@ func _send_identify():
   }
   _websocket.put_packet(JSON.stringify(payload).to_utf8_buffer())
 
+# delete this
+func on_ready():
+  # print("mi id: %s" % user.id)
+  pass
+
 func _event_handler(payload: Dictionary):
   # Reference https://discord.com/developers/docs/events/gateway-events#payload-structure
   var event: String = payload["t"]
@@ -101,6 +115,8 @@ func _event_handler(payload: Dictionary):
     user = DiscordUser.new()
     user.id = int(data["user"]["id"])
     user.name = data["user"]["username"]
+    await get_guilds()
+    debugPrint("Bot is ready")
     bot_ready.emit()
     
   elif event == "MESSAGE_CREATE":
@@ -109,6 +125,7 @@ func _event_handler(payload: Dictionary):
     message.content = data["content"]
     message.author = DiscordUser.new()
     message.author.id = int(data["author"]["id"])
+
     # For some reason global_name returns null here
     # ^- From kruz: maybe it is an intent?  I see my user when
     # printing the global_name, but I'm using more intents
@@ -142,7 +159,51 @@ func _event_handler(payload: Dictionary):
     command_request.caller.name = data["member"]["user"]["username"]
     command_request.caller.mention = "<@%s>" % command_request.caller.id
     command_used.emit(command_request)
-    
+
+func get_guilds()->void:
+  var url = "https://discord.com/api/v9/users/@me/guilds"
+  var headers = [
+    "Authorization: Bot %s" % token,
+    "Content-Type: application/json"
+  ]
+  var http_req = HTTPRequest.new()
+  DiscordRequestHandler.add_child(http_req)
+  http_req.request_completed.connect(func(_r, _c, _h, _b): print("request completed"); http_req.queue_free())
+  var error = http_req.request(url, headers, HTTPClient.METHOD_GET)
+
+  if error == OK:
+    var response = await http_req.request_completed
+
+    var code = response[1]
+    var body:PackedByteArray = response[3]
+
+    if code == 200:
+      var json:JSON = JSON.new()
+      json.parse(body.get_string_from_utf8())
+      for i:int in json.get_data().size():
+        var obj:Dictionary = json.get_data()[i]
+        var id:String = obj.get("id")
+        if id:
+
+          var guild:DiscordGuild = DiscordGuild.new()
+
+          guild.name = obj.get("name") as String
+          guild.id = obj.get("id") as String
+          # obj.get("icon").get_class()
+          var icon = obj.get("icon")
+          if icon:
+            guild.icon = obj.get("icon")
+          guild.permissions = obj.get("permissions") as int
+          var features:Array = obj.get("features")
+          if features.size() > 0:
+            for _feature:String in features:
+              guild.features.append(_feature)
+          guild.owner = obj.get("owner") as bool
+          
+          guilds.append(guild)
+          print(obj)
+          debugPrint("Added guild: %s" % id)
+
 func _heartbeat():
   # Reference https://discord.com/developers/docs/events/gateway#sending-heartbeats
   var data: Dictionary = {"op": 1, "d": sequence_number or null}
@@ -173,19 +234,124 @@ func _start_heartbeat(interval: float):
 ##     discord_bot.register_slash_command("bye", "Says goodbye", options)
 ## [/codeblock]
 func register_slash_command(command_name: String, description: String, options: Array = []):
-  if guild_id.is_empty():
-    push_error("guild_id is empty. If your command does not register, enable Developer Mode in Discord, copy the Server ID, and set it in the 'guild_id' field.");
-  var url = "https://discord.com/api/v9/applications/%s/guilds/%s/commands" % [user.id, guild_id]
+  for currentGuild:DiscordGuild in guilds:
+    # if guild_id.is_empty():
+      # push_error("guild_id is empty. If your command does not register, enable Developer Mode in Discord, copy the Server ID, and set it in the 'guild_id' field.");
+    var url = "https://discord.com/api/v9/applications/%s/guilds/%s/commands" % [user.id, currentGuild.id]
+    var headers = [
+      "Authorization: Bot %s" % token,
+      "Content-Type: application/json"
+    ]
+    var payload = {
+      "name": command_name,
+      "description": description,
+      "options": options
+    }
+    var http_req = HTTPRequest.new()
+    DiscordRequestHandler.add_child(http_req)
+    http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
+    http_req.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+    debugPrint("Registered command: %s on guild: %s" %[command_name, currentGuild])
+
+
+
+
+  pass
+  # return []
+
+## message -> The message to send
+## channel -> The channel to send the message to
+## embed -> Optional field used to send a discord embed
+func send_message(message:String, channel:DiscordChannel, embed:Dictionary = {})->void:
+  if channel == null: return
+  var url = "https://discord.com/api/v9/channels/%s/messages" % channel.id
   var headers = [
     "Authorization: Bot %s" % token,
     "Content-Type: application/json"
   ]
-  var payload = {
-    "name": command_name,
-    "description": description,
-    "options": options
+  var payload:Dictionary = {
+    "content": message
   }
+  if embed.size() > 0:
+    payload["embeds"] = [embed]
   var http_req = HTTPRequest.new()
   DiscordRequestHandler.add_child(http_req)
   http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
   http_req.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+  pass
+
+func get_server_channels(guild:DiscordGuild)->Array[DiscordChannel]:
+
+  if token.is_empty():
+    push_error("TOKEN is empty. Cannot fetch channels.")
+    return []
+
+  var channels:Array[DiscordChannel]
+
+  var url = "https://discord.com/api/v9/guilds/%s/channels" % guild.id
+  var headers = [
+      "Authorization: Bot %s" % token 
+  ]
+    
+  var http_req = HTTPRequest.new()
+  DiscordRequestHandler.add_child(http_req)
+  http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
+  
+  var error = await http_req.request(url, headers, HTTPClient.METHOD_GET)
+  if error == OK:
+    var response:Array = await http_req.request_completed
+    var result:int = response[0]
+    var code:int = response[1]
+    var receivedHeaders:PackedStringArray = response[2]
+    var body:PackedByteArray = response[3]
+
+    if code == 200:
+      var json:JSON = JSON.new()
+      var parse_error:int = json.parse(body.get_string_from_utf8())
+
+      if parse_error == OK:
+        var data:Array = json.get_data()
+        for i:int in data.size():
+          var obj:Dictionary = data[i]
+          var parentID = obj.get("parent_id")
+          if parentID != null:
+            var channel:DiscordChannel = DiscordChannel.new()
+            channel.id = obj.get("id")
+            channel.type = obj.get("type")
+            channel.name = obj.get("name")
+            channel.nsfw = obj.get("nsfw")
+            channel.rate_limit_per_user = obj.get("rate_limit_per_user")
+            channels.append(channel)
+  return channels
+
+func get_channel_of_name(guild:DiscordGuild, name:String)->DiscordChannel:
+  var channel:DiscordChannel
+  var channels:Array[DiscordChannel] = await get_server_channels(guild)
+  for _c:DiscordChannel in channels:
+    if _c.name == name:
+      channel = _c
+  return channel
+
+func get_channel_of_id(guild:DiscordGuild, id:int)->DiscordChannel:
+  var channel:DiscordChannel
+  var channels:Array[DiscordChannel] = await get_server_channels(guild)
+  for _c:DiscordChannel in channels:
+    if _c.id == id:
+      channel = _c
+  return channel
+
+func get_guild_by_name(guild_name:String)->DiscordGuild:
+  var guild:DiscordGuild
+  for _g:DiscordGuild in guilds:
+    if _g.name == guild_name:
+      guild = _g
+  return guild
+
+
+# dunno why somebody would need this, but if someone does, here it is
+func get_guild_by_id(id:String)->DiscordGuild:
+  var guild:DiscordGuild
+  for _g:DiscordGuild in guilds:
+    if _g.id == id:
+      guild = _g
+  return guild
--- addons/godiscord/scripts/discord_channel.gd
+++ addons/godiscord/scripts/discord_channel.gd
@@ -4,6 +4,11 @@ extends DiscordResource
 
 ## ID of the channel
 var id: int
+var type:int
+var name:String
+var nsfw:bool
+var rate_limit_per_user:float
+
 
 ## Send a message to the channel. [br]
 ## [b]Example:[/b]
@@ -13,15 +18,30 @@ var id: int
 ##         message.channel.send_message("You said: " + message.content)
 ## [/codeblock]
 func send_message(content: String):
-	var url = "https://discord.com/api/v9/channels/%s/messages" % id
-	var headers = [
-		"Authorization: Bot %s" % token,
-		"Content-Type: application/json"
-	]
-	var payload = {
-		"content": content
-	}
-	var http_request = HTTPRequest.new()
-	DiscordRequestHandler.add_child(http_request)
-	http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
-	http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
+  var url = "https://discord.com/api/v9/channels/%s/messages" % id
+  var headers = [
+    "Authorization: Bot %s" % token,
+    "Content-Type: application/json"
+  ]
+  var payload = {
+    "content": content
+  }
+  var http_request = HTTPRequest.new()
+  DiscordRequestHandler.add_child(http_request)
+  http_request.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+  http_request.request_completed.connect(func(_r, _c, _h, _b): http_request.queue_free())
+
+func sendMessage(message:String)->void:
+  var url = "https://discord.com/api/v9/channels/%s/messages" % id
+  var headers = [
+    "Authorization: Bot %s" % token,
+    "Content-Type: application/json"
+  ]
+  var payload = {
+    "content": message
+  }
+  var http_req = HTTPRequest.new()
+  DiscordRequestHandler.add_child(http_req)
+  http_req.request_completed.connect(func(_r, _c, _h, _b): http_req.queue_free())
+  http_req.request(url, headers, HTTPClient.METHOD_POST, JSON.stringify(payload))
+  pass
